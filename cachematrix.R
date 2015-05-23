5d782ad79ca39897d2fc4b95f7fa430e90953734
dpingel1 <dpingel87@gmail.com>
2015-05-23 00:56:04 -0400

    Adjustments

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,19 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  inv = NULL
-  set = function(y) {
+  inv <- NULL
+  set <- function(y) {
     
     x <<- y
     inv <<- NULL
   }
-  get = function() x
-  setinv = function(inverse) inv <<- inverse 
-  getinv = function() inv
-  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse 
+  getinv <- function() inv
+  list(set = set, 
+       get = get, 
+       setinv = setinv, 
+       getinv = getinv)
 }
 
 
@@ -23,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     
-    inv = x$getinv()
+    inv <- x$getinv()
     
     if (!is.null(inv)){
       
@@ -31,8 +34,8 @@ cacheSolve <- function(x, ...) {
       return(inv)
     }
     
-    mat.data = x$get()
-    inv = solve(mat.data, ...)
+    mat.data <- x$get()
+    inv <- solve(mat.data, ...)
     
     x$setinv(inv)
     
